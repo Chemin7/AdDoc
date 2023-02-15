7b4508b28cf42a31eb425a03d13169e54748a350
Emanuel Vazquez <evazquezrosales@gmail.com>
2023-02-15 16:06:56 -0600

    creacion de router para /citas

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
-
+const citasRouter = require('./routes/citas')
 app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
+
+app.use('/citas',citasRouter);
 
 app.listen(PORT);
 
---
+++ routes/citas.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    //res.render('citas.ejs');
+})
+
+router.post('/',(req,res,next)=>{
+    const nombre_paciente = req.params.nombre;
+    const email_paciente = req.parms.email;
+    const telefono_paciente = req.params.paciente;
+
+    res.render('',{nombre:nombre_paciente, email:email_paciente,telefono:telefono_paciente});
+});
+
+modoule.exports = router;
