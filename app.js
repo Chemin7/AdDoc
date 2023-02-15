53d66146849398c6a3af512e3a4ec3db049e6997
Emanuel Vazquez <evazquezrosales@gmail.com>
2023-02-15 16:45:47 -0600

    creacion de path a public

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const citasRouter = require('./routes/citas')
+
 app.get('/',(req,res)=>{
     res.render('login.ejs')
 })
