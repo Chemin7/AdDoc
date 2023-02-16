09a76d0810003d9faefc672fe6695998d7a12c0b
Emanuel Vazquez <evazquezrosales@gmail.com>
2023-02-16 08:15:57 -0600

    add ejs as a default engine

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const citasRouter = require('./routes/citas')
 
 app.get('/',(req,res)=>{
-    res.render('login.ejs')
+    //res.render('login.ejs')
+    res.render('citas');
 })
 
 app.use('/citas',citasRouter);
